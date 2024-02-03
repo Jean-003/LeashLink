11f9624cb786371a5f26468ffb5b4a0fcda65363
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-02-03 11:52:57 +0000

    Testing api which has been linked to search button, and returning event data. CORS error occuring to be resolved

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
  // function ticketMaster() {
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events/G5diZfkn0B-bh.json?apikey=idVREd0toy5AGDXaGZhf07ksmoaUk7kx";
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=idVREd0toy5AGDXaGZhf07ksmoaUk7kx"
+    //  "https://app.ticketmaster.com/discovery/v2/events/G5diZfkn0B-bh.json?apikey=idVREd0toy5AGDXaGZhf07ksmoaUk7kx";
+
+    
+
     // var apiKey = "idVREd0toy5AGDXaGZhf07ksmoaUk7kx"
     // var keySecret = "n2kW26Zszi44O68v"
 
@@ -19,8 +23,6 @@
   .catch(error => {//handle any errors
   });
 }
-
-    // Create CODE HERE to Log the queryURL
 function fetchData() {
   console.log(queryURL) //log the url
   fetch(queryURL)
@@ -30,11 +32,11 @@ function fetchData() {
   var eventName = data.name;   //access event name property from data(test)
   console.log(eventName) // log the name 
 
-// display the names in console
+
 data.events.forEach(
   event => {
     console.log (event.name);
-  }
+  } // display the names in console
 );
 
   return data; // return the data 
@@ -46,3 +48,32 @@ data.events.forEach(
 }
 
 //test git push
+
+// function fetchData() {
+//   console.log(queryURL); // Logging the URL
+
+//   // Created a script element to set the jsonp url and a callback parameter
+//   var script = document.createElement('script');
+
+//   // using the callback function that will handle the JSONP response
+//   window.handleResponse = function(data) {
+//     console.log(data); // logging the data
+
+//     var eventName = data.name; // Access the event name property from data
+//     console.log(eventName); // Log the name
+
+//     // Display the names in the console
+//     data.events.forEach(function(event) {
+//       console.log(event.name);
+//     });
+
+//     // Clean up by removing the script element
+//     document.body.removeChild(script);
+//   };
+
+//   // Set the source of the script element to the JSONP URL
+//   script.src = queryURL + '&callback=handleResponse';
+
+//   // Append the script element to the document body
+//   document.body.appendChild(script);
+// }
