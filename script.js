b1eb4e57f29041c044713e97c9c7b3884c7d8492
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-02-03 09:43:01 +0000

    event api+key added and function to ca on this added and linked to search button

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-function initMap() {
-    // Get the user's location
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        function (position) {
-          var userLocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-  
-          console.log('User location:', userLocation);
-  
-          // Map centered on the user's location
-          var map = new google.maps.Map(document.getElementById('map'), {
-            center: userLocation,
-            zoom: 12
-          });
-  
-          // Marker for the user's location
-          new google.maps.Marker({
-            position: userLocation,
-            map: map,
-            title: 'Your Location',
-            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-          });
-  
-          // Geocode the latitude and longitude to get the city name
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode({ location: userLocation }, function (results, status) {
-            if (status === 'OK' && results[0]) {
-              var cityName = results[0].address_components[2].long_name; 
-              console.log('City Name: ' + cityName);
-            } else {
-              console.error('Unable to geocode location');
-            }
-          });
-        },
-        function (error) {
-          console.error('Error getting user location:', error.message);
-        }
-      );
-    }
+ // function ticketMaster() {
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events/G5diZfkn0B-bh.json?apikey=idVREd0toy5AGDXaGZhf07ksmoaUk7kx";
+    // var apiKey = "idVREd0toy5AGDXaGZhf07ksmoaUk7kx"
+    // var keySecret = "n2kW26Zszi44O68v"
+
+  document.addEventListener ("DOMContentLoaded", function () {
+    document.getElementById("fetchButton").addEventListener("click",fetchData);
+    });
+
+  function fetchData() {
+    fetch(queryURL)
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+    //parse the data or do something else 
+    return response.json();
+  })
+
+  .catch(error => {//handle any errors
+  });
+}
+
+    // Create CODE HERE to Log the queryURL
+function fetchData() {
+  console.log(queryURL) //log the url
+  fetch(queryURL)
+  .then(response =>response.json())
+.then(data => {  //access and use data 
+  console.log(data); //log the data 
+  var eventName = data.name;   //access event name property from data(test)
+  console.log(eventName) // log the name 
+
+// display the names in console
+data.events.forEach(
+  event => {
+    console.log (event.name);
   }
-  
+);
+
+  return data; // return the data 
+})
+
+.catch(error => {//handle any errors
+  console.error(error);
+});
+}
